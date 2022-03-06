9435a24c6ead6fa37f59dd0ad9185c88bd300354
tetsumote <unionproject88@gmail.com>
2022-03-06 22:59:12 +0100

    add starter

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-x*n4-vw$o(7-fis5ye)4pd!*!vl52r(v5beb-@4%p++#tr*jvu'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
